9615e214f39fb204e3a9f6be4aa708c8c8b7be9e
snovakovic <stefan.novakovich@gmail.com>
2020-01-10 18:35:00 +0100

    1560 Remove quill from dependencies

--- src/modules/toolbarFactory.js
+++ src/modules/toolbarFactory.js
@@ -1,6 +1,6 @@
-import IconAlignLeft from 'quill/assets/icons/align-left.svg';
-import IconAlignCenter from 'quill/assets/icons/align-center.svg';
-import IconAlignRight from 'quill/assets/icons/align-right.svg';
+import IconAlignLeft from '../../assets/align-left.svg';
+import IconAlignCenter from '../../assets/align-center.svg';
+import IconAlignRight from '../../assets/align-right.svg';
 import { BaseModule } from './BaseModule';
 
 export default function toolbarFactory(Quill) {
